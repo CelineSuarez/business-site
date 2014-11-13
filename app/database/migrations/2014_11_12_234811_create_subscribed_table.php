065f97c94560370b4c20fb1e73f01b89179736ae
Celine <celinesuarez7@gmail.com>
2014-11-13 00:08:25 +0000

    updating database and tables

---
+++ app/database/migrations/2014_11_12_234811_create_subscribed_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribedTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('subscribed', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->string('email');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('subscribed');
+	}
+
+}
